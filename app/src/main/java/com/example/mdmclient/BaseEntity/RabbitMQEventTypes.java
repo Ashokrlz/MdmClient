20dfbfa2ef2036410b6794e101afaf074c42e670
Ashok_Sundar <ashokrlz736@gmail.com>
2019-11-08 12:30:41 +0300

    Event Typo fix

--- app/src/main/java/com/example/mdmclient/BaseEntity/RabbitMQEventTypes.java
+++ app/src/main/java/com/example/mdmclient/BaseEntity/RabbitMQEventTypes.java
@@ -2,8 +2,8 @@ package com.example.mdmclient.BaseEntity;
 
 public class RabbitMQEventTypes {
 
-        public static final String INCIDENT_EVENT = "PTT";
-        public static final String PTT_EVENT = "IncidentComment";
+        public static final String INCIDENT_EVENT = "IncidentComment";
+        public static final String PTT_EVENT = "PTT";
         public static final String LOCATION_UPDATE_EVENT = "LocationUpdate";
         public static final String SOS_EMERGENCY_EVENT = "EMERGENCY";
 }
